e1abd7176020da8027285cdbafef2b5c01d6bbff
Greg Kempe <greg@kempe.net>
2022-06-24 15:45:55 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# django-sass-processor-dart-sass
+
+This is a shim that makes [django-sass-processor](https://pypi.org/project/django-sass-processor/) use
+[Dart Sass](https://sass-lang.com/dart-sass) rather than [libsass](https://sass-lang.com/libsass) because
+libsass is deprecated.
+
+This provides a replacement for `sass.py` provided by libsass and simply calls out to `sass` provided by Dart Sass.
+Dart Sass must be [installed separately](https://sass-lang.com/install).
+
+## Note: vanilla Dart Sass only
+
+This does not support the custom functions provided by django-sass-processor through libsass. It only supports vanilla
+Dart Sass.
+
+## How to use it
+
+This clashes with libsass, so remove libsass if you have it installed:
+
+```
+pip uninstall libsass
+```
+
+Install Dart Sass by following their [instructions](https://sass-lang.com/install).
+
+Install this package:
+
+```bash
+pip install https://github.com/laws-africa/django-sass-processor-dart-sass
+```
+
+That's it. Install and use `django-sass-processor` as normal.
+
+# Advanced used
+
+If you need to change the `sass` command, override it like so:
+
+```python
+from sass import dart_sass
+dart_sass.sass_command = '/special/sass'
+```
---
+++ sass.py
@@ -0,0 +1,49 @@
+import subprocess
+from tempfile import NamedTemporaryFile
+
+from django.conf import settings
+
+
+class DartSass:
+    """ A shim that replaces libsass with Dart Sass, primarily aimed at django-sass-processor.
+    """
+    sass_command = 'sass'
+
+    def compile(self, filename, include_paths, **kwargs):
+        # build up include paths
+        includes = [['-I', x] for x in include_paths]
+        includes = [p for pp in includes for p in pp]
+
+        with NamedTemporaryFile() as outf:
+            cmd = [
+                getattr(settings, 'SASS_COMMAND', None) or self.sass_command,
+                filename + ":" + outf.name,
+                *includes,
+                ]
+
+            try:
+                subprocess.run(cmd, check=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+            except subprocess.CalledProcessError as e:
+                raise CompileError(e.stdout.decode('utf-8'))
+
+            content = outf.read()
+            with open(outf.name + '.map', 'rb') as f:
+                sourcemap = f.read()
+
+        if 'source_map_filename' in kwargs:
+            return content, sourcemap
+
+        return content
+
+
+class SassFunction:
+    def __init__(self, *args, **kwargs):
+        pass
+
+
+class CompileError(ValueError):
+    pass
+
+
+dart_sass = DartSass()
+compile = dart_sass.compile
